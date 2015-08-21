90ba847bbda8ffd806059e43eb4d602fc0a4fe19
Esteban Muñoz <esteban.ucr@gmail.com>
2015-08-21 15:09:12 -0700

    Fix Java and PHP wrappers to return the correct exit status

--- test/test.php
+++ test/test.php
@@ -15,6 +15,15 @@
 	/**
 	 * This file provides examples of all the methods in the Test class of this wrapper
 	 */
+
+	// This method is used to format the output of the examples above
+	function display_response($response, $title)
+	{
+		// Let's print the title followed by a empty line
+		echo '<br/>'.$title.'<br/><br/>';
+		echo print_r($response);
+		echo '<br/><br/>End of '.$title.'<br/>';
+	}
 	
 	// Let's create a contact
 	try {
@@ -26,8 +35,8 @@
 		// Let's display the response of the request using the display_response function defined at the end of this file
 		display_response($contact, 'Create a contact');
 	} catch(Exception $e) {
-		print_r("Error occurred when executing get_bitly_short_url: " . $e->getMessage() . "\n" . $e->getTraceAsString());
-		return -1;
+		print_r("Error occurred when creating contact: " . $e->getMessage() . "\n" . $e->getTraceAsString());
+		exit(1);
 	}
 	
 	// Let's see your contacts
@@ -35,8 +44,8 @@
 		$contacts = VerticalResponse\API\Test::get_contacts();
 		display_response($contacts, 'List your contacts');
 	} catch(Exception $e) {
-		print_r("Error occurred when executing get_bitly_short_url: " . $e->getMessage() . "\n" . $e->getTraceAsString());
-		return -1;
+		print_r("Error occurred when listing contacts: " . $e->getMessage() . "\n" . $e->getTraceAsString());
+		exit(1);
 	}
 	
 	// Let's get the details of one of your contacts
@@ -45,8 +54,8 @@
 		$details = VerticalResponse\API\Test::get_contact_details($contact);
 		display_response($details, 'Get the details of one of your contacts');
 	} catch(Exception $e) {
-		print_r("Error occurred when executing get_bitly_short_url: " . $e->getMessage() . "\n" . $e->getTraceAsString());
-		return -1;
+		print_r("Error occurred when getting details of a contact: " . $e->getMessage() . "\n" . $e->getTraceAsString());
+		exit(1);
 	}
 	
 	// Let's create a list
@@ -58,16 +67,16 @@
 		);
 		display_response($list, 'Create a list');
 	} catch(Exception $e) {
-		print_r("Error occurred when executing get_bitly_short_url: " . $e->getMessage() . "\n" . $e->getTraceAsString());
-		return -1;
+		print_r("Error occurred when creating list: " . $e->getMessage() . "\n" . $e->getTraceAsString());
+		exit(1);
 	}
 	// Let's see all your lists
 	try {
 		$lists = VerticalResponse\API\Test::get_lists();
 		display_response($lists, 'Get your lists');
 	} catch(Exception $e) {
-		print_r("Error occurred when executing get_bitly_short_url: " . $e->getMessage() . "\n" . $e->getTraceAsString());
-		return -1;
+		print_r("Error occurred when getting lists: " . $e->getMessage() . "\n" . $e->getTraceAsString());
+		exit(1);
 	}
 	
 	// Let's see the details of one of your lists
@@ -77,8 +86,8 @@
 		$details = VerticalResponse\API\Test::list_details($list);
 		display_response($details, 'Get details of one of your lists');
 	} catch(Exception $e) {
-		print_r("Error occurred when executing get_bitly_short_url: " . $e->getMessage() . "\n" . $e->getTraceAsString());
-		return -1;
+		print_r("Error occurred when getting details of a list: " . $e->getMessage() . "\n" . $e->getTraceAsString());
+		exit(1);
 	}
 	
 	// Let's see the contacts that belong to one of your lists
@@ -87,8 +96,8 @@
 		$contacts = VerticalResponse\API\Test::get_lists_contacts($list);
 		display_response($contacts, 'Get the contacts of one of your lists');
 	} catch(Exception $e) {
-		print_r("Error occurred when executing get_bitly_short_url: " . $e->getMessage() . "\n" . $e->getTraceAsString());
-		return -1;
+		print_r("Error occurred when getting contacts of a list: " . $e->getMessage() . "\n" . $e->getTraceAsString());
+		exit(1);
 	}
 	
 	// Let's create a new contact in one of your lists
@@ -101,18 +110,9 @@
 		);
 		display_response($contact, 'Create a contact in a list');
 	} catch(Exception $e) {
-		print_r("Error occurred when executing get_bitly_short_url: " . $e->getMessage() . "\n" . $e->getTraceAsString());
-		return -1;
+		print_r("Error occurred when creating contact in a list: " . $e->getMessage() . "\n" . $e->getTraceAsString());
+		exit(1);
 	}
 	
-	return 0;
-	
-	// This method is used to format the output of the examples above
-	function display_response($response, $title)
-	{
-		// Let's print the title followed by a empty line
-		echo '<br/>'.$title.'<br/><br/>';
-		echo print_r($response);
-		echo '<br/><br/>End of '.$title.'<br/>';
-	}
+	exit(0);
 ?>
